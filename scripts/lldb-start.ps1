e95ed1ec201c5b016d2a98d7346c9b98ecddc830
Francesc Elies <francesc.elies@mbbm-ast.com>
2022-01-27 06:46:26 +0100

    problem: environment variables are modified for current shell too

--- scripts/lldb-start.ps1
+++ scripts/lldb-start.ps1
@@ -1,8 +1,24 @@
 $repoPath =  $MyInvocation.MyCommand.Path | split-path | split-path
-$env:PATH="$repoPath\llvm\bin;$repoPath\llvm\lib\clang\13.0.0\lib\windows;$env:PATH"
 
-$env:PATH="C:\Python36-x64;C:\Python36-x64\Scripts;$env:PATH"
-$env:PYTHONHOME="C:\Python36-x64\"
-$env:PYTHONPATH="C:\Python36-x64\Lib"
+try {
+    # Temporarily set environment variables
+    $prevPath = $env:PATH
+    $prevPythonHome = $env:PYTHONHOME
+    $prevPythonPath = $env:PYTHONPATH
 
-lldb ./src/py_file_run.exe C:\Python39-x64 .\test.py
+
+    $env:PATH="$repoPath\llvm\bin;$repoPath\llvm\lib\clang\13.0.0\lib\windows;$env:PATH"
+
+    $env:PATH="C:\Python36-x64;C:\Python36-x64\Scripts;$env:PATH"
+    $env:PYTHONHOME="C:\Python36-x64\"
+    $env:PYTHONPATH="C:\Python36-x64\Lib"
+
+    # lldb works with python 3.6 only, thus environment variables above
+    lldb ./src/py_file_run.exe C:\Python39-x64 .\test.py
+
+} finally {
+    # restore the previous values
+    $env:PATH = $prevPath
+    $env:PYTHONHOME = $prevPythonHome
+    $env:PYTHONPATH = $prevPythonPath
+}
